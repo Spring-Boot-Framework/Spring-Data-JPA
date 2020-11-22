7ec1069d6fed5af0cdcefd24787aaf22645a8dfd
Anantha Raju C <arcswdev@gmail.com>
2020-11-22 22:00:59 +0530

    ADDED few JPA Queries

--- src/main/java/io/github/anantharajuc/sdjpa/SpringDataJpaApplication.java
+++ src/main/java/io/github/anantharajuc/sdjpa/SpringDataJpaApplication.java
@@ -1,12 +1,14 @@
 package io.github.anantharajuc.sdjpa;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.anantharajuc.sdjpa.model.Person;
 import io.github.anantharajuc.sdjpa.repositories.PersonRepository;
 import lombok.extern.log4j.Log4j2;
 
@@ -26,5 +28,21 @@ public class SpringDataJpaApplication implements CommandLineRunner
 	public void run(String... args) throws Exception 
 	{
 		log.info("Application started at {}", LocalDateTime.now());	
+		
+		log.info("personRepository.count() : "+ personRepository.count());		
+		
+		Person person;
+		
+		person = personRepository.getPersonByName("John Doe");
+		
+		log.info("John Doe person.getId() : "+ person.getId());	
+		
+		log.info("personRepository.existsByUsername(\"johndoe\") : "+ personRepository.existsByUsername("johndoe"));	
+		
+		Optional<Person> optionalPerson;
+		
+		optionalPerson = personRepository.findByUsernameOrEmailPrimary("johndoe", "johndoe@t-online.de");
+		
+		log.info("optionalPerson.isPresent() : "+ optionalPerson.isPresent());	
 	}
 }
--- src/main/java/io/github/anantharajuc/sdjpa/repositories/PersonRepository.java
+++ src/main/java/io/github/anantharajuc/sdjpa/repositories/PersonRepository.java
@@ -1,7 +1,14 @@
 package io.github.anantharajuc.sdjpa.repositories;
 
+import java.util.Optional;
+
+import javax.validation.constraints.NotBlank;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import io.github.anantharajuc.sdjpa.model.Person;
 
@@ -13,5 +20,24 @@ import io.github.anantharajuc.sdjpa.model.Person;
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long>
 {
-
+	/*
+	 * Retrieve a {@link Person} from the data store by name.
+	 * @param name the name to search for
+	 * @return The {@link Person} if found
+	 */
+	@Query(value = "SELECT * FROM person WHERE name = :name",nativeQuery = true) 
+	@Transactional(readOnly=true)
+	Person getPersonByName(@Param("name") String name);
+	
+	/*
+     * Retrieve a {@link Person} from the data store by username or email.
+     * @param username the username to search for or
+     * @param email the email to search for
+     * @return the {@link Person} if found
+     */
+	@Transactional(readOnly = true)
+	Optional<Person> findByUsernameOrEmailPrimary(String username, String email);
+	
+	@Transactional(readOnly = true)
+	Boolean existsByUsername(@NotBlank String username);
 }
